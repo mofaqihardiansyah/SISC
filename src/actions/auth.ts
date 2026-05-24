1faf47cb143d5d10bd2bb0ab426e1b2504bfa452
Mochamad Faqih Ardiansyah <mofaqihardiansyah@gmail.com>
2026-05-24 15:58:02 +0700

    update: auth OTP

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -23,7 +23,9 @@ async function sendOTPEmail(email: string, code: string) {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // use TLS
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
